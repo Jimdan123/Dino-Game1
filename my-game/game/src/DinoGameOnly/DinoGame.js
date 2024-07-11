83b48d00cc89859b6d0c6401ce7b006fd5d350be
HuyLe61 <ledinhhuy02082006@gmail.com>
2024-07-11 17:21:58 +0700

    Dino Setup

--- my-game/game/src/DinoGameOnly/DinoGame.js
+++ my-game/game/src/DinoGameOnly/DinoGame.js
@@ -1,71 +1,232 @@
-var dl = {
+
+var dl = cc.Layer.extend({
     helloLabel:null,
     sprite:null,
     spriteDino:null,
+    jumpHeight: 200,
+    jumpDuration: 0.3, 
+    gameState: "init",
+    dinoState: "run",
     init:function () 
     {
         //////////////////////////////
         // 1. super init first
         this._super();
         //cc.log("MyLayer - init");
-        foo();
+        if ('keyboard' in cc.sys.capabilities) {
+            var keyboardListener = {
+                event: cc.EventListener.KEYBOARD, 
+
+                onKeyPressed: function(key, event) 
+                {
+                    var t = event.getCurrentTarget();
+                    t.onKeyPressed(key);
+                },
+
+                onKeyReleased: function(key, event) 
+                {
+                    var t = event.getCurrentTarget();
+                    t.onKeyReleased(key);
+                }
+            };
+            cc.eventManager.addListener(keyboardListener, this);
+        } 
         // get screen size
-        var size = cc.director.getWinSize();
-        /////////////////////////////
-        // 2. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        this.helloLabel = new cc.LabelTTF("Hello World", "Impact", 38);
-        // position the label on the center of the screen
-        this.helloLabel.setPosition(size.width / 2, size.height - 40);
-        // add the label as a child to this layer
-        this.addChild(this.helloLabel, 5);
+        //var size = cc.director.getWinSize();
 
-        this.createDino();
-
-        // add "Helloworld" splash screen"
-        this.sprite = new cc.Sprite(s_HelloWorld);
-        this.sprite.setAnchorPoint(0.5, 0.5);
-        this.sprite.setPosition(size.width / 2, size.height / 2);
-        this.sprite.setScale(size.height / this.sprite.getContentSize().height);
-        this.addChild(this.sprite, 0);
-
+        this.gameOn();
     },
-    createDino: function()
+        
+    gameOn: function()
     {
         cc.spriteFrameCache.addSpriteFrames(dinoPos, dino);
-        var spriteDino = new cc.Sprite("#dino_duck_1.png");
-        spriteDino.setPosition(200, 200);
-        spriteDino.setAnchorPoint(0.5,0.5);
-        this.addChild(spriteDino, 1);
+        this.spriteDino = new cc.Sprite("#dino_jump.png"); 
+        this.spriteDino.setPosition(200, 200);
+        this.spriteDino.setAnchorPoint(0.5,0.5); 
+        this.addChild(this.spriteDino, 0);
+        
+        // Setup animation
+        this.setupDino();
+
+        // Run by default
+        this.run();
+
     },
+
+    onKeyPressed: function(key)
+    {
+        // Jump
+        if (key === cc.KEY.space) 
+        {
+            if (this.dinoState == "run") 
+            {
+                this.dinoState = "jump";
+                this.jump();
+            }
+
+            cc.log("Key space pressed");
+        }
+        else if (key === cc.KEY.down) 
+        {
+            if (this.dinoState == "run")
+            {
+                this.dinoState = "duck";
+                this.duck();
+            }
+
+            else if (this.dinoState == "jump") {
+                this.cancelJump();
+            }
+            
+            cc.log("Key down pressed");
+        }
+        else 
+        { 
+            this.dinoState = "run";
+            cc.log("run");
+        }
+    },
+
+    onKeyReleased: function(key)
+    {
+        if (key === cc.KEY.space) 
+        {   
+            if (this.dinoState == "jump") {
+                //this.spriteDino.moveTo(200, 200);
+                //this.dinoState = "run";
+                //this.run();
+                cc.log("Run after Jump");
+            }
+            cc.log("Key space released");
+
+            if (this.dinoState == "run") {
+                cc.log("Running");
+            }
+            
+        }
+        else if (key === cc.KEY.down) 
+        {
+            if (this.dinoState == "duck")
+            {
+                this.cancelJump();
+                this.dinoState = "run";
+                this.run();
+            }
+        
+            cc.log("Key down released");
+        }
+    },
+
+    setupDino: function() {
+        // Run animation
+        this.animRun = new cc.Animation();
+        for (var i = 1; i <= 2; i++) {
+            var frameName = "dino_run_" + i + ".png";
+            var frame = cc.spriteFrameCache.getSpriteFrame(frameName);
+            this.animRun.addSpriteFrame(frame); 
+        }
+        this.animRun.setDelayPerUnit(0.1);
+         
+        // Jump animation
+        this.animJump = new cc.Animation(); 
+        this.animJump.addSpriteFrame(cc.spriteFrameCache.getSpriteFrame("dino_jump.png"));
+        //this.animJump = cc.sequence(jumpUp, jumpDown);
+
+        
+        // Duck animation
+        this.animDuck = new cc.Animation();
+        for (var i = 1; i <= 2; i++) {
+            var frameName = "dino_duck_" + i + ".png";
+            var frame = cc.spriteFrameCache.getSpriteFrame(frameName);
+            this.animDuck.addSpriteFrame(frame); 
+        }
+        this.animDuck.setDelayPerUnit(0.1);
+        this.animDuck.setLoops(999999);
+        
+    },
+
+    jump: function() {
+        this.spriteDino.stopAllActions();
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight));
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight));
+        var jumpMotion = cc.sequence(jumpUp, jumpDown, cc.callFunc(this.endJump, this));
+        this.spriteDino.runAction(cc.animate(this.animJump));
+        this.spriteDino.runAction(jumpMotion);
+    },
+
+    cancelJump: function() 
+    {
+        this.spriteDino.stopAllActions();
+
+        this.dinoState = "vol"; 
+
+        var lop = cc.moveTo(0.1, cc.p(200, 200));
+
+        var a = cc.sequence(lop, cc.callFunc(this.duck, this));
+        this.spriteDino.runAction(a);
+        //var cancleMotion = cc.sequence(cc.moveTo(200, 200), cc.callFunc(this.duck), this);
+        //this.spriteDino.runAction(cancleMotion);
+       
+
+    endJump: function() {
+            this.dinoState = "run";
+            this.run();
+    },
+
+    duck: function() 
+    {
+        this.spriteDino.stopAllActions();
+        this.spriteDino.runAction(cc.animate(this.animDuck));
+    },
+
+    run: function() 
+    {
+        this.spriteDino.stopAllActions();
+        this.spriteDino.runAction(cc.repeatForever(cc.animate(this.animRun))); 
+    },
+
     onEnter: function()
     {
         this._super();
         cc.log("MyLayer - onEnter()");
         this.scheduleUpdate();
     },
+
     update: function(dt)
     {
-        cc.log ("MyLayer - onEnter()" + dt);
-    },
-};
-var dinoLayer = cc.Layer.extend(dl);
-    
-    var dinoScene = cc.Scene.extend({
-        onEnter:function () 
-        {
-            // 1. super init first
-            this._super();
-            // 2. new layer
-            var layer = new dinoLayer();
-            // 3. init layer
-            layer.init();
-            // 4. add to scene
-            this.addChild(layer);
-            cc.log("MyLayer - onEnter()");
-            this.scheduleUpdate();
-            cc.log("Node Parent: " + dino.parent);
+        /*
+        if (this.dinoState === "runInit") {
+            this.run();
+            this.dinoState = "run"
         }
-    });
+        else if (this.dinoState === "jump") {
+            this.jump();
+        }
+        else if (this.dinoState === "duck") {
+            this.duck();
+        }
+        */
+    },
+
+});
+
+var dinoScene = cc.Scene.extend({
+    onEnter:function () 
+    {
+        // 1. super init first
+        this._super();
+         // 2. new layer
+        var layer = new dl();
+        // 3. init layer
+        layer.init();
+        // 4. add to scene
+        this.addChild(layer);
+
+        //cc.log("MyLayer - onEnter()");
+        this.scheduleUpdate();
+        //cc.log("Node Parent: " + dino.parent);
+    }
+});
+
+cc.director.runScene(new dinoScene());
     
